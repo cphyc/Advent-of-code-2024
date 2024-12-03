8d15d4ec658101e762786489d389989b07a67a3f
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-03 10:45:02 +0100

    Solve day 2, II

--- day2/solve.py
+++ day2/solve.py
@@ -1,3 +1,4 @@
+from copy import copy
 from pathlib import Path
 import numpy as np
 
@@ -15,7 +16,7 @@ data = (Path(__file__).parent / "input").read_text()
 def parse(data: str) -> list[list[int]]:
     return [[int(_) for _ in line.split()] for line in data.splitlines()]
 
-def part1(data: np.ndarray) -> int:
+def part1(data: list[list[int]]) -> int:
     count = 0
     for report in data:
         diff = np.diff(report)
@@ -26,8 +27,22 @@ def part1(data: np.ndarray) -> int:
     print(f"Part 1: {count}")
     return count
 
-def part2(data: np.ndarray) -> int:
-    pass
+def part2(data: list[list[int]]) -> int:
+    count = 0
+    for report in data:
+        for i in range(len(report)):
+            subreport = copy(report)
+            if i >= 0:
+                subreport.pop(i)
+            diff = np.diff(subreport)
+            sign = np.sign(diff[0])
+            if np.all(diff * sign >= 1) & np.all(diff * sign <= 3):
+                count += 1
+                break
+
+    print(f"Part 2: {count}")
+    return count
 
 
 part1(parse(data))
+part2(parse(data))
