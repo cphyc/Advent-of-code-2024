669f51f357db35a0edeb512b201a92bb54a08e0a
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-03 10:29:50 +0100

    Solve day 1, II

--- day1/solve.py
+++ day1/solve.py
@@ -10,13 +10,28 @@ test_data = """\
 3   9
 3   3"""
 
+def parse(data):
+    return np.array([[int(_) for _ in line.split()] for line in data.splitlines()])
+
 def part1(data):
-    data = np.array([[int(_) for _ in line.split()] for line in data.splitlines()])
+    data = parse(data)
 
     L, R = data.T
     L, R = np.sort(L), np.sort(R)
 
     print(f"Part 1: {np.abs(R - L).sum()}")
 
+def part2(data):
+    data = parse(data)
+
+    L, R = data.T
+
+    count = 0
+    for n in L:
+        count += n * (R == n).sum()
+
+    print(f"Part 2: {count}")
+
 data = Path("input").read_text()
 part1(data)
+part2(data)
