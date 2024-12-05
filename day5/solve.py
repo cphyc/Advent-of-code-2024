f8c9d48efbdb596941956650dda8b80512ef4076
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-05 11:07:24 +0100

    Solve day 5, II

--- day5/solve.py
+++ day5/solve.py
@@ -1,5 +1,6 @@
 import enum
 from pathlib import Path
+from tqdm import tqdm
 
 class State(enum.Enum):
     RULE=1
@@ -60,9 +61,26 @@ def _check_rule(rules_tuple: list[tuple[int, int]], update: list[int]) -> bool:
     for i, orderi in enumerate(update):
         for orderj in update[:i]:
             if (orderi, orderj) in rules_tuple:
-                # print(f"{orderi=} -> {orderj=} breaks the rules")
                 return False
     return True
+
+def _check_and_reorder(rules_tuple: list[tuple[int, int]], update: list[int]) -> list[int]:
+    new_update = update.copy()
+    while not _check_rule(rules_tuple, new_update):
+        for i, orderi in enumerate(new_update):
+            break_now = False
+            for orderj in new_update[:i]:
+                # Swap i and i-1
+                if (orderi, orderj) in rules_tuple:
+                    new_update[i-1], new_update[i] = new_update[i], new_update[i-1]
+                    break_now = True
+                    break
+
+            if break_now:
+                break
+
+    return new_update
+
 
 def part1(rules: dict[int, int], rules_tuple: list[tuple[int, int]], updates: list[list[int]]):
     total = 0
@@ -72,6 +90,16 @@ def part1(rules: dict[int, int], rules_tuple: list[tuple[int, int]], updates: li
 
     print(f"Part 1: {total}")
 
+def part2(rules: dict[int, int], rules_tuple: list[tuple[int, int]], updates: list[list[int]]):
+    total = 0
+    for update in tqdm(updates):
+        new_update = _check_and_reorder(rules_tuple, update)
+        if any(new_update[i] != update[i] for i in range(len(update))):
+            total += new_update[len(new_update)//2]
+
+    print(f"Part 2: {total}")
+
 rules, inverted_rules, updates = parse(data)
 
 part1(rules, inverted_rules, updates)
+part2(rules, inverted_rules, updates)
