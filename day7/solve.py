e4bb40f3ceccf3338aab1702c8580a71965dfca1
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-07 12:03:31 +0100

    Solve day 7, I

---
+++ day7/solve.py
@@ -0,0 +1,45 @@
+from itertools import product
+from operator import add, mul
+from pathlib import Path
+
+test_data = """\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+
+data = (Path(__file__).parent / "input").read_text()
+
+def parse(data: str) -> dict[int, list[int]]:
+    ret = {}
+
+    for line in data.splitlines():
+        k, v = line.split(": ")
+        ret[int(k)] = [int(x) for x in v.split()]
+
+    return ret
+
+def part1(equations: dict[int, list[int]]):
+    ok: dict[int, bool] = {}
+
+    for key, values in equations.items():
+        ok[key] = False
+        tot = values[0]
+        for operations in product((mul, add), repeat=len(values) - 1):
+            tot = values[0]
+            for op, val in zip(operations, values[1:]):
+                tot = op(tot, val)
+
+            if tot == key:
+                ok[key] = True
+                break
+
+    print(f"Part 1: {sum(k for k, v in ok.items() if v)}")
+
+equations = parse(data)
+part1(equations)
