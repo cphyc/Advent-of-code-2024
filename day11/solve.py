409f121ae6ee33c6332be7f380163a85ed6bb5cf
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-12 08:29:27 +0100

    Solve day 11, II

--- day11/solve.py
+++ day11/solve.py
@@ -1,4 +1,7 @@
+from functools import cache
+from itertools import chain
 from pathlib import Path
+from tqdm import tqdm
 
 
 test_data = "0 1 10 99 999"
@@ -26,3 +29,23 @@ gems = parse(data)
 for i in range(25):
     gems = blink(gems)
 print(f"Part 1: {len(gems)}")
+
+@cache
+def compute(i: int):
+    gems = [i]
+    for _ in range(25):
+        gems = blink(gems)
+
+    return gems
+
+
+# Not efficient, but works in ~5min
+twosteps = chain.from_iterable(compute(j) for i in tqdm(data) for j in compute(i))
+sss = set(twosteps)
+
+my_len = {}
+for i in tqdm(sss):
+    my_len[i] = len(compute(i))
+
+twosteps = chain.from_iterable(compute(j) for i in tqdm(data) for j in compute(i))
+print(f"Part 2: {sum(my_len[i] for i in twosteps)}")
