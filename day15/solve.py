034b3504dc4983a0de7fec40e7b01b454816a659
Corentin Cadiou <corentin.cadiou@cphyc.me>
2024-12-23 21:13:27 +0100

    Solve day 23, II

--- day15/solve.py
+++ day15/solve.py
@@ -40,6 +40,7 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
 
 data = (Path(__file__).parent / "input").read_text()
 
+
 class Movement(enum.Enum):
     LEFT = (-1, 0)
     UP = (0, -1)
@@ -55,11 +56,14 @@ class Movement(enum.Enum):
             "<": cls.LEFT,
         }[c]
 
+
 class Stuff(enum.Enum):
     WALL = "#"
     BOX = "O"
     EMPTY = "."
     ROBOT = "@"
+    BBOXL = "["
+    BBOXR = "]"
 
     @classmethod
     def from_char(cls, c: str) -> "Stuff":
@@ -118,7 +122,74 @@ class Map:
         elif box == Stuff.WALL:
             return Map(self.objects, old_pos)
 
-    def print(self):
+    def move2(self, mv: Movement):
+        old_pos = self.robot_position
+        new_pos = (old_pos[0] + mv.value[0], old_pos[1] + mv.value[1])
+
+        if new_pos not in self.objects:
+            # New position is empty
+            return Map(self.objects, new_pos)
+        # Otherwise, get a stack of boxes to push
+        next_pos = new_pos
+        box_stack = []
+
+        # If moving up/down, all big boxes need to align
+        if mv in (Movement.UP, Movement.DOWN):
+            first_box = self.objects.get(next_pos, Stuff.EMPTY)
+            factor = 1
+        else:
+            first_box = Stuff.BBOXL if mv == Movement.RIGHT else Stuff.BBOXR
+            factor = 2
+        if first_box == Stuff.BBOXL:
+            next_posL, next_posR = next_pos, (next_pos[0] + 1, next_pos[1])
+        else:
+            next_posL, next_posR = (next_pos[0] - 1, next_pos[1]), next_pos
+
+        boxL, boxR = None, None
+
+        # Get stack of BIG box to be moved
+        while (
+            (
+                (boxL := self.objects.get(next_posL, Stuff.EMPTY)),
+                (boxR := self.objects.get(next_posR, Stuff.EMPTY))
+            ) == (Stuff.BBOXL, Stuff.BBOXR)
+        ):
+            box_stack.append(next_posL)
+            box_stack.append(next_posR)
+
+            next_posL = (next_posL[0] + mv.value[0] * factor, next_posL[1] + mv.value[1] * factor)
+            next_posR = (next_posR[0] + mv.value[0] * factor, next_posR[1] + mv.value[1] * factor)
+
+        # If we're moving sideways, need to subtract one step
+        if mv in (Movement.RIGHT, Movement.LEFT):
+            next_posL = (next_posL[0] - mv.value[0], next_posL[1] - mv.value[1])
+            next_posR = (next_posR[0] - mv.value[0], next_posR[1] - mv.value[1])
+            boxL = self.objects.get(next_posL, Stuff.EMPTY)
+            boxR = self.objects.get(next_posR, Stuff.EMPTY)
+
+        ok = (
+            ((boxL, boxR) == (Stuff.BBOXR, Stuff.EMPTY) and mv == Movement.RIGHT) or
+            ((boxL, boxR) == (Stuff.EMPTY, Stuff.BBOXL) and mv == Movement.LEFT) or
+            ((boxL, boxR) == (Stuff.EMPTY, Stuff.EMPTY) and mv in (Movement.UP, Movement.DOWN))
+        )
+        if ok:
+            # If we hit an empty space, move the boxes
+            new_box_stack: list[tuple[tuple[int, int], Stuff]] = []
+            for p in box_stack:
+                s = self.objects.pop(p)
+                new_box_stack.append(
+                    ((p[0] + mv.value[0], p[1] + mv.value[1]), s)
+                )
+            # Add back the boxes at shifted positions
+            for p, s in new_box_stack:
+                self.objects[p] = s
+            return Map(self.objects, new_pos)
+        else:
+            # Can't move if we hit a wall/unaligned boxes
+            return Map(self.objects, old_pos)
+
+
+    def print(self, mvmt: Movement=None):
         imax = max(self.objects.keys(), key=lambda ij: ij[0])[0]
         jmax = max(self.objects.keys(), key=lambda ij: ij[1])[1]
 
@@ -128,13 +199,24 @@ class Map:
                 obj = self.objects.get((i, j), Stuff.EMPTY)
 
                 if (i, j) == self.robot_position:
-                    chars += "@"
+                    c = {
+                        None: "@",
+                        Movement.LEFT: "<",
+                        Movement.RIGHT: ">",
+                        Movement.UP: "^",
+                        Movement.DOWN: "v",
+                    }[mvmt]
+                    chars += c
                 elif obj == Stuff.EMPTY:
                     chars += "."
                 elif obj == Stuff.BOX:
                     chars += "O"
                 elif obj == Stuff.WALL:
                     chars += "#"
+                elif obj == Stuff.BBOXL:
+                    chars += "["
+                elif obj == Stuff.BBOXR:
+                    chars += "]"
             print(chars)
 
     def get_score(self) -> int:
@@ -146,6 +228,26 @@ class Map:
             score += key[1] * 100 + key[0]
         return score
 
+    def double(self) -> "Map":
+        new_objects: dict[tuple[int, int], Stuff] = {}
+
+        for key, val in self.objects.items():
+            key1 = (2 * key[0] + 0, key[1])
+            key2 = (2 * key[0] + 1, key[1])
+            val1, val2 = val, val
+            if val == Stuff.WALL:
+                val1, val2 = Stuff.WALL, Stuff.WALL
+            elif val == Stuff.BOX:
+                val1, val2 = Stuff.BBOXL, Stuff.BBOXR
+
+            new_objects[key1] = val1
+            new_objects[key2] = val2
+
+        new_pos = self.robot_position
+        new_pos = (2*new_pos[0], new_pos[1])
+
+        return Map(new_objects, new_pos)
+
 
 def parse(data: str) -> tuple[Map, list[Movement]]:
     map_str, movement_str = data.split("\n\n")
@@ -155,6 +257,7 @@ def parse(data: str) -> tuple[Map, list[Movement]]:
 
     return object_map, movements
 
+
 om, movements = parse(data)
 
 for m in movements:
@@ -163,3 +266,14 @@ for m in movements:
     # om.print()
 
 print(f"Part 1: {om.get_score()}")
+
+
+om, movements = parse(test_data)
+om = om.double()
+
+om.print()
+
+for i, m in enumerate(movements):
+    print(f" {i=:4d} {m=} ".center(40, "="))
+    om.print(m)
+    om = om.move2(m)
--- day23/solve.py
+++ day23/solve.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from itertools import combinations
 from pathlib import Path
 import networkx as nx
 
@@ -38,22 +40,101 @@ td-yn
 
 data = (Path(__file__).parent / "input").read_text()
 
+
 def parse(data: str) -> nx.Graph:
     g = nx.Graph()
-    g.add_edges_from(
-        line.split("-") for line in data.splitlines()
-    )
+    g.add_edges_from(line.split("-") for line in data.splitlines())
     return g
+
 
 def part1(g: nx.Graph):
     # Find 3-loops in the graph
-    candidates = sorted(
-        truple for truple in nx.simple_cycles(g, 3)
-        if any(node.startswith("t") for node in truple)
+    candidates = helper(g, list(set((_,)) for _ in g.nodes), 3)
+    t_candidates = [
+        truple for truple in candidates if any(node.startswith("t") for node in truple)
+    ]
+    print(f"Part 1: {len(t_candidates)}")
+    return candidates
+
+
+
+def helper(g: nx.Graph, nuples: list[set[str]], max_depth: int) -> list[set[str]]:
+    # print(f"Length: {len(nuples[0])}, found {len(nuples)=}")
+    if len(nuples[0]) == max_depth:
+        return nuples
+
+    muples = []
+    # Try all combinations of n-uples
+    for s1, s2 in combinations(nuples, 2):
+        new1 = list(s1 - s2)
+        new2 = list(s2 - s1)
+
+        if not (len(new1) == len(new2) == 1):
+            continue
+
+        n1 = new1[0]
+        n2 = new2[0]
+
+        # Make sure new1 is connected to all of s2
+        ok = all(g.has_edge(n1, old2) for old2 in s2)
+        if not ok:
+            continue
+        ok &= all(g.has_edge(n2, old1) for old1 in s1)
+        if not ok:
+            continue
+
+        s12 = s1.union({n2})
+        if s12 not in muples:
+            muples.append(s12)
+
+    if len(muples) == 0:
+        return nuples
+
+    all_nodes = reduce(set.union, muples)
+
+    current_degree = len(muples[0])
+    new_g = nx.Graph()
+    new_g.add_edges_from(
+        (ni, nj)
+        for ni, nj in g.edges
+        if (
+            ni in all_nodes
+            and nj in all_nodes
+            and g.degree(ni) >= current_degree
+            and g.degree(nj) >= current_degree
+        )
     )
-    print(f"Part 1: {len(candidates)}")
+
+    # print(f"Started with {g}, now {new_g}")
+
+    return helper(new_g, muples, max_depth)
+
+def part2(g: nx.Graph):
+    max_len = 0
+    biggest_boy = []
+    for n in list(g.nodes):
+        candidates = set(g.neighbors(n))
+        candidates.add(n)
+        g2 = nx.Graph()
+        g2.add_edges_from(
+            (ni, nj) for ni, nj in g.edges if ni in candidates or nj in candidates
+        )
+        ret = helper(g2, list(set((_,)) for _ in g2.nodes), 100)
+        if ret and len(ret[0]) > max_len:
+            max_len = len(ret[0])
+            biggest_boy = ret
+            print(f"New max: {max_len}")
+        if len(ret[0]) == 13:
+            break
+        else:
+            g.remove_node(n)
+
+    answer = ",".join(sorted(biggest_boy[0]))
+    print(f"Part 2: {answer}")
 
 
 g = parse(data)
 
+# truples = part1(g)
 part1(g)
+part2(g)
